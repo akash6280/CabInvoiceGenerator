ccbb5c020c25b48ea34be71687f13b2678f3c487
Akash Singh <akashsingh6280@gmail.com>
2021-09-23 02:06:31 +0530

    ADDED: method to calculate fare of multiple ride

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -12,4 +12,13 @@ public class CabInvoiceGenerator {
 		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
 		return Math.max(totalFare, MINIMUM_FARE);
 	}
+
+	public double calculateFare(Ride rides[]) {
+		double totalFare = 0;
+		for(Ride ride:rides) {
+			totalFare += this.calculateFare(ride.distance,ride.time);
+		}
+		return totalFare;
+
+	}
 }
