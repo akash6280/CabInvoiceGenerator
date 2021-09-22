bb1569114f309270f63b0da5ee07929b8aac58a9
Akash Singh <akashsingh6280@gmail.com>
2021-09-23 04:19:53 +0530

    REFACTORED: added enum for normal and premium user

--- src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -4,10 +4,17 @@ public class Ride {
 
 	public double distance;
 	public int time;
-	
-	public Ride(double distance, int time) {
+	public enum RideType {
+		PREMIUM_RIDE,
+		NORMAL_RIDE
+	}
+	public RideType rideType;
+	public Ride(double distance, int time,RideType ridetype) {
 		super();
 		this.distance = distance;
 		this.time = time;
+		this.rideType=ridetype;
+		
 	}
+	
 }
