000fa9782d29aa90c6250c8119d1d0f4d937c0d1
Akash Singh <akashsingh6280@gmail.com>
2021-09-23 01:37:58 +0530

    REFACTORED: calculate max of minimum fare and total fare

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -6,9 +6,10 @@ package com.bridgelabz.cabinvoicegenerator;
 public class CabInvoiceGenerator {
 	private static final double MINIMUM_COST_PER_KM = 10;
 	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
 	
 	public double calculateFare(double distance, int time) {
-		return distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
-		
+		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+		return Math.max(totalFare, MINIMUM_FARE);
 	}
 }
